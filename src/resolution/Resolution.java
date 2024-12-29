ed0bbf5c05348baa519098fc7e4ca83b78813dfa
Kleber Vales <ksvales@outlook.pt>
2024-12-29 10:04:35 -0300

    Resolution

--- src/resolution/Resolution.java
+++ src/resolution/Resolution.java
@@ -1,10 +1,48 @@
 package resolution;
 
-public class Resolution {
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Result {
+
+	public static List<Integer> reverseArray(List<Integer> a) {
+
+		List<Integer> reversed = new ArrayList<>();
+
+		for (int i = a.size() - 1; i >= 0; i--) {
+			reversed.add(a.get(i));
+		}
+
+		return reversed;
 
 	}
 
+}
+
+public class Resolution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
+
+		List<Integer> res = Result.reverseArray(arr);
+
+		bufferedWriter.write(res.stream().map(Object::toString).collect(joining(" ")) + "\n");
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
 }
